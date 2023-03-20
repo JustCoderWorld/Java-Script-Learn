c83a930fd19c5b274de20f7ade3aaf067b0bff8e
JustCoder <128308653+JustCoderWorld@users.noreply.github.com>
2023-03-21 01:43:14 +0300

    PRIVATE PRACC
    Son of Villager Website pracc

---
+++ MAIN PRIVATE PRACC/script.js
@@ -0,0 +1,38 @@
+let All = document.getElementById("All");
+
+let YesResultText = document.getElementById("YesResultText").innerHTML;
+document.getElementById("YesResultText").innerHTML = "";
+
+let NoResultText = document.getElementById("NoResultText").innerHTML;
+document.getElementById("NoResultText").innerHTML = "";
+
+function direction(ID, location, time) {
+  if (time == 0) {
+    window.location.href = location;
+    return;
+  }
+  document.getElementById(ID).innerHTML = time + "";
+  time--;
+  setTimeout(function () {
+    direction(ID, location, time);
+  }, 1000);
+}
+
+
+document.getElementById("YesBtn").onclick = function () {
+  document.getElementById("YesResultText").innerHTML = YesResultText;
+  All.innerHTML = " ";
+  direction("counter", "https://youtu.be/AqFj62v-PKY?t=17", 15);
+};
+
+document.getElementById("NoBtn").onclick = function () {
+  document.getElementById("NoResultText").innerHTML = NoResultText;
+  All.innerHTML = " ";
+  direction("counter", "https://youtu.be/4Fttg4dY2QQ?t=12", 15);
+};
+
+// |||| J U
+// |||||||||||||| ST
+// ||||||||||||||||||||||| FE
+// |||||||||||||||||||||||||||||||||||| EL
+// ||||||||||||||||||||||||||||||||||||||||||||||||| Development
