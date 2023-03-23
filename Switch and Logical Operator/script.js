d5a3f3628451c2ca19314132a3c563f5bb18af43
JustCoder <128308653+JustCoderWorld@users.noreply.github.com>
2023-03-23 05:36:32 +0300

    ValorantRank Site

---
+++ Switch and Logical Operator/script.js
@@ -0,0 +1,47 @@
+var ValorantRank = document.getElementById("ValorantRankText");
+document.getElementById("SubmitBtn").onclick = function () {
+  switch (ValorantRank.value) {
+    case "Iron":
+      window.location.href = "https://youtu.be/BWKxd2GWVms?t=9";
+      break;
+    case "Bronze":
+      alert("Contact me I'll buy you a prosthetic hand");
+      break;
+    case "Silver":
+      alert("Looks like a New Player ");
+      break;
+    case "Gold":
+      alert("U need some prac");
+      break;
+    case "Platinum":
+      alert("MEHHH ... You have potential");
+      break;
+    case "Diamond":
+      alert("You are experienced ");
+      break;
+    case "Ascedant":
+      alert("OoooOOO You are Team Player");
+      break;
+    case "Immortal":
+      window.location.href = "https://www.youtube.com/watch?v=SgscTppV91s";
+      break;
+    case "Radiant":
+      window.location.href = "https://www.youtube.com/watch?v=w5AquDBS5W0&t=5s";
+      break;
+    default:
+      break;
+  }
+};
+
+// && AND
+// || OR
+// ! False > True 
+// ! True > False
+
+
+// <!-- 
+// |||| J U
+// |||||||||||||| ST 
+// ||||||||||||||||||||||| FE
+// |||||||||||||||||||||||||||||||||||| EL
+// ||||||||||||||||||||||||||||||||||||||||||||||||| Development -->
